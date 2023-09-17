ec83136a141cb9c3e301e056ad13dc10f64bf14e
Omelchenko Michael <socanime@gmail.com>
2023-09-17 14:16:57 +0300

    add example app_support

---
+++ example/app_support/src/models/__init__.py
@@ -0,0 +1,14 @@
+from share.models import Base
+from .support import Category, Support
+from .user import Permission, User
+
+
+__all__ = (
+    "Base",
+    "Category",
+    "Support",
+    "Permission",
+    "User",
+)
+
+

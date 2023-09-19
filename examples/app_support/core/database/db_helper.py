7a30c9eee3481bc71a3e9008e1dd836ae479b812
Omelchenko Michael <socanime@gmail.com>
2023-09-19 21:14:12 +0300

    Updated the db_helper in the app_support

---
+++ examples/app_support/core/database/db_helper.py
@@ -0,0 +1,67 @@
+from asyncio import current_task
+from contextlib import asynccontextmanager
+
+from sqlalchemy.ext.asyncio import (
+    AsyncSession,
+    create_async_engine,
+    async_sessionmaker,
+    async_scoped_session)
+
+from core.config import settings
+
+
+class DatabaseHelper:
+    def __init__(self, url: str, echo: bool = False):
+        self.engine = create_async_engine(url=url, echo=echo)
+
+        self.session_factory = async_sessionmaker(
+            bind=self.engine,
+            autoflush=False,
+            autocommit=False,
+            expire_on_commit=False
+        )
+
+    def get_scope_session(self):
+        return async_scoped_session(
+            session_factory=self.session_factory,
+            scopefunc=current_task
+        )
+
+    async def get_session(self) -> AsyncSession:
+        async with self.session_factory() as session:
+            yield session
+            await session.close()
+
+    async def get_session_dependency(self) -> AsyncSession:
+        session = self.get_scope_session()
+        yield session
+        await session.close()
+
+    # async def get_db_session(self):
+    #     from sqlalchemy import exc
+    #
+    #     async with self.session_factory() as session:
+    #         try:
+    #             yield session
+    #             await session.commit()
+    #         except exc.SQLAlchemyError as error:
+    #             await session.rollback()
+    #             raise
+    #         finally:
+    #             await session.close()
+    @asynccontextmanager
+    async def get_db_session(self):
+        from sqlalchemy import exc
+
+        session = self.session_factory()
+        try:
+            yield session
+            await session.commit()
+        except exc.SQLAlchemyError as error:
+            await session.rollback()
+            raise
+        finally:
+            await session.close()
+
+
+db_helper = DatabaseHelper(settings.db_url, settings.db_echo)

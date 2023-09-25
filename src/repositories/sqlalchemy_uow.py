da0d91d326ecb954c0a57e63e24a9075f05a175c
Omelchenko Michael <socanime@gmail.com>
2023-09-25 12:01:23 +0300

    updated uow, generic

--- src/repositories/sqlalchemy_uow.py
+++ src/repositories/sqlalchemy_uow.py
@@ -1,27 +1,4 @@
-from abc import abstractmethod, ABC
-
-
-class UnitOfWork(ABC):
-
-    @abstractmethod
-    def __init__(self):
-        ...
-
-    @abstractmethod
-    async def __aenter__(self):
-        ...
-
-    @abstractmethod
-    async def __aexit__(self, *args):
-        ...
-
-    @abstractmethod
-    async def commit(self):
-        ...
-
-    @abstractmethod
-    async def rollback(self):
-        ...
+from .base_uow import UnitOfWork
 
 
 class SqlAlchemyUnitOfWork(UnitOfWork):
--- src/services/generic_service.py
+++ src/services/generic_service.py
@@ -1,5 +1,5 @@
-from .schemas import PyModel
-from .repository import AbstractRepository, SqlModel
+from ..schemas.base_schema import PyModel
+from ..repositories.sqlalchemy_repository import AbstractRepository, ModelType
 
 
 class BaseService:
@@ -7,14 +7,14 @@ class BaseService:
     def __init__(self, repository: AbstractRepository) -> None:
         self.repository: AbstractRepository = repository
 
-    async def create(self, model: PyModel) -> SqlModel:
+    async def create(self, model: PyModel) -> ModelType:
         return await self.repository.create(data=model.model_dump())
 
-    async def update(self, pk: int, model: PyModel) -> SqlModel:
+    async def update(self, pk: int, model: PyModel) -> ModelType:
         return await self.repository.update(data=model.model_dump(), id=pk)
 
     async def delete(self, pk: int) -> None:
         await self.repository.delete(id=pk)
 
-    async def get(self, pk: int) -> SqlModel:
+    async def get(self, pk: int) -> ModelType:
         return await self.repository.get(id=pk)
